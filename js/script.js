269cc7270eadce28a5c0a7b3227718f5dd55d050
quintusdev <quintusdev92@gmail.com>
2023-05-23 13:42:04 +0200

    [FEATURES][STYLE]Fine MILESTONE1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// 
+// DICHIARAZIONE VARIABILI
+let imgElement = document.querySelector('.image');
+let imgContent = "";
+
+//INSERIMENTO IMMAGINI IN UN ARRAY
+const images = ['../img/01.webp', '../img/02.webp', '../img/03.webp', '../img/04.webp','../img/05.webp'];
+
+//CICLO FOR PER INSERIRE LE IMMAGINI ALL'INTERNO DEL CONTENITORE HTML
+for (let i = 0; i < images.length; i++) {
+    let currentImg = images[i];
+
+    imgContent += '<div class="image"><img src="${currentImg}"></div>';
+}
+
+//VISUALIZZO LE IMMAGINI IN HTML
+imgElement.innerHTML = imgContent;
+
+//INSERISCO ISTRUZIONI PER FAR SCORRERE LE IMMAGINI PREMENDO SUI PULSANTI
+
